9013e6b8137c7fc3c28c0c34fc19851b85c04847
Prabhs920 <prabhnoors920@gmail.com>
2025-03-30 09:50:54 -0400

    Add ProductListViewController.swift

---
+++ A2_iOS_Prabhnoor_101415063/ProductListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ProductListViewController.swift
+//  A2_iOS_Prabhnoor_101415063
+//
+//  Created by Prabhnoor on 26.03.25.
+//
+
+import UIKit
+import CoreData
+
+class ProductListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var products: [Product] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchProducts()
+    }
+
+    func fetchProducts() {
+        let request: NSFetchRequest<Product> = Product.fetchRequest()
+        do {
+            products = try context.fetch(request)
+            tableView.reloadData()
+        } catch {
+            print("Failed to fetch products: \(error)")
+        }
+    }
+
+    // MARK: - TableView Data Source
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        let product = products[indexPath.row]
+        cell.textLabel?.text = product.name
+        cell.detailTextLabel?.text = product.desc
+        return cell
+    }
+}
