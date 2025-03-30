b5980326bca14bb568baa83ef2f795154141f9e9
Prabhs920 <prabhnoors920@gmail.com>
2025-03-30 09:32:48 -0400

    setup code base

---
+++ Product+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  A2_iOS_Prabhnoor_101415063
+//
+//  Created by Prabhnoor on 26.03.25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class ProductObj: NSManagedObject {
+
+}
